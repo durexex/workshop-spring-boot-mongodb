01097c8b34ec68d3a1f15505c65e2bdc843280ee
durexex <eduardodurieuxlopes@gmail.com>
2022-02-28 18:07:54 -0300

    First commit

---
+++ src/test/java/com/devdurex/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdurex.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
