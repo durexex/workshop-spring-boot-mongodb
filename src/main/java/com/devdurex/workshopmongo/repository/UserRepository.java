e7a03f5ba2310961ecb41c3d62bb165c3c2a15e2
durexex <eduardodurieuxlopes@gmail.com>
2022-02-28 21:41:37 -0300

    Connected to mongoDB Repo and service

---
+++ src/main/java/com/devdurex/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devdurex.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devdurex.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository <User, String> {
+
+}
