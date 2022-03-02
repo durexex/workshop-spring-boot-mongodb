256b10b70ff1f4734783915516c4256e800c8849
durexex <eduardodurieuxlopes@gmail.com>
2022-03-02 13:13:04 -0300

    Entity post with user

--- src/main/java/com/devdurex/workshopmongo/config/Instantiation.java
+++ src/main/java/com/devdurex/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.devdurex.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.devdurex.workshopmongo.domain.Post;
 import com.devdurex.workshopmongo.domain.User;
+import com.devdurex.workshopmongo.repository.PostRepository;
 import com.devdurex.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,30 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User schummacher = new User(null, "Schumacher", "schumi@gmail.com");
 		User barrichello = new User(null, "Rubens Barrichello", "barrica@gmail.com");
 		User massa = new User(null, "Felipe Massa", "massa@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", massa);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Happy day today", barrichello);
+		
+				
 		userRepository.saveAll(Arrays.asList(schummacher, barrichello, massa));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
