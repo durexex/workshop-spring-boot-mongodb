1e26cd04bdc80258c18be87479ddff49f5d08610
durexex <eduardodurieuxlopes@gmail.com>
2022-03-02 13:21:41 -0300

    Entity post with userDTO

--- src/main/java/com/devdurex/workshopmongo/config/Instantiation.java
+++ src/main/java/com/devdurex/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.devdurex.workshopmongo.domain.Post;
 import com.devdurex.workshopmongo.domain.User;
+import com.devdurex.workshopmongo.dto.AuthorDTO;
 import com.devdurex.workshopmongo.repository.PostRepository;
 import com.devdurex.workshopmongo.repository.UserRepository;
 
@@ -37,11 +38,12 @@ public class Instantiation implements CommandLineRunner {
 		User barrichello = new User(null, "Rubens Barrichello", "barrica@gmail.com");
 		User massa = new User(null, "Felipe Massa", "massa@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", massa);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Happy day today", barrichello);
+		userRepository.saveAll(Arrays.asList(schummacher, barrichello, massa));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(massa));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Happy day today", new AuthorDTO(barrichello));
 		
 				
-		userRepository.saveAll(Arrays.asList(schummacher, barrichello, massa));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
