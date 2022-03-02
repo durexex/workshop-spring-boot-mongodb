c9260821cb904d1f6873362f1660e0e45078bafb
durexex <eduardodurieuxlopes@gmail.com>
2022-03-01 21:19:33 -0300

    delete user DELETET

--- src/main/java/com/devdurex/workshopmongo/services/UserService.java
+++ src/main/java/com/devdurex/workshopmongo/services/UserService.java
@@ -36,6 +36,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findbyId(id);
+		repo.deleteById(id);
+	}
+	
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
