c18be602a9f7a14f521d1f8c97949bd0b7f43b31
durexex <eduardodurieuxlopes@gmail.com>
2022-03-02 13:36:29 -0300

    User Posts endpoint

--- src/main/java/com/devdurex/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devdurex/workshopmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devdurex.workshopmongo.domain.Post;
 import com.devdurex.workshopmongo.domain.User;
 import com.devdurex.workshopmongo.dto.UserDTO;
 import com.devdurex.workshopmongo.services.UserService;
@@ -63,6 +64,14 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findbyId(id);
+				
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 
 	
 }
